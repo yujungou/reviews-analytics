c6248a6b07521a26eff82ac634e714dd5cfffe7f
Sherry Ou <sherrym711sr@gmail.com>
2023-06-24 15:48:47 +0800

    second read.py

--- read.py
+++ read.py
@@ -6,4 +6,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
+print('All done. There are totally', len(data), 'data')
 
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('The average length of reviews is ', sum_len/len(data))
