2bb758eeeb182daaf6cf72a3e809b99834dee938
Sherry Ou <sherrym711sr@gmail.com>
2023-06-25 10:25:48 +0800

    improved version

--- read.py
+++ read.py
@@ -20,3 +20,15 @@ for d in data:
 	if len(d) < 100:
 		new.append(d)
 print('There are', len(new),'data whose length less than 100.')
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('There are', len(good), 'reviews that mention good')
+
+good = [d for d in data if 'good' in d]
+print(good)
+
+bad = ['bad' in d for d in data]
+print(bad)
